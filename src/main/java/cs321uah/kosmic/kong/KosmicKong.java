300738ef64aee0187d83e786a026860541881888
Aeyu17 <jami3507@gmail.com>
2024-10-09 16:37:23 -0500

    initialise maven project

---
+++ src/main/java/cs321uah/kosmic/kong/KosmicKong.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cs321uah.kosmic.kong;
+
+/**
+ *
+ * @author jamie
+ */
+public class KosmicKong {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
