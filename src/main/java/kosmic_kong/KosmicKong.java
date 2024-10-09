7eda05eb8b42456891badd270db42184bdc3af49
Aeyu17 <jami3507@gmail.com>
2024-10-09 17:21:29 -0500

    Add home screen functionality

--- src/main/java/kosmic_kong/KosmicKong.java
+++ src/main/java/kosmic_kong/KosmicKong.java
@@ -2,15 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package cs321uah.kosmic.kong;
+package kosmic_kong;
+
+import displays.HomeScreen;
 
 /**
  *
- * @author jamie
+ * @author Jamie Roberson
  */
 public class KosmicKong {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // runs GUI in an async queue
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HomeScreen().setVisible(true);
+            }
+        });
+        
+        System.out.println("GUI is running.");
     }
 }
